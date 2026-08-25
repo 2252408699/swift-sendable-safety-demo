dd3661b9e1e4d09a3448aa10042a3d4b01e7a908
2252408699 <49366744+2252408699@users.noreply.github.com>
2026-08-25 14:30:30 +0800

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,8 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "SendableSafetyDemo",
+    platforms: [.macOS(.v13)],
+    targets: [.executableTarget(name: "SendableSafetyDemo")]
+)
